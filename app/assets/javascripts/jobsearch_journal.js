a426aaf065c8eb8b2e3129da190c37e03fcdc314
krisko6 <cpauley@calpoly.edu>
2015-06-06 11:25:39 -0700

    first commit

---
+++ app/assets/javascripts/jobsearch_journal.js
@@ -0,0 +1,13 @@
+window.JobsearchJournal = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  JobsearchJournal.initialize();
+});
